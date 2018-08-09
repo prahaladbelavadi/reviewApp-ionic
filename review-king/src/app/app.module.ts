75876b653c6b5564945c1e7935ec5f7697c37810
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-09 09:10:16 -0700

    mod

--- review-king/src/app/app.module.ts
+++ review-king/src/app/app.module.ts
@@ -1,32 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ReviewsProvider } from '../providers/reviews/reviews';
+import { AddReviewPage } from '../pages/add-review-page/add-review-page';
+import { Reviews } from '../providers/reviews';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddReviewPage
   ],
   imports: [
-    BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddReviewPage
   ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ReviewsProvider
-  ]
+  providers: [Reviews]
 })
 export class AppModule {}
